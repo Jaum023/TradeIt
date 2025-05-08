4cbd91240fe93921759ad3849cac98fb1350b99a
Jaum023 <123989291+Jaum023@users.noreply.github.com>
2025-05-07 21:20:01 -0300

    basic login page and reusable input text widget

--- tradeit_app/lib/src/features/auth/presentation/controllers/auth_controller.dart
+++ tradeit_app/lib/src/features/auth/presentation/controllers/auth_controller.dart
@@ -1,2 +1,18 @@
 // 'StateNotifier/Notifier com lógica de UI'
+import 'package:flutter/material.dart';
 
+class AuthController {
+  final txtEmail = TextEditingController();
+  final txtPassword = TextEditingController();
+
+  Future<void> login(BuildContext context) async {
+    // Aqui você chamaria o usecase ou repositório de login no futuro
+    print('Login com: ${txtEmail.text} / ${txtPassword.text}');
+    Navigator.pushReplacementNamed(context, "/chat");
+  }
+
+  void dispose() {
+    txtEmail.dispose();
+    txtPassword.dispose();
+  }
+}
