a6d333acfd2e6d248c6399e24a8155adfaa381f4
Rafael Pereira <rafael_pereira5@outlook.com>
2025-05-30 16:01:55 -0300

    trazendo o nome do criador do usuario

--- tradeit_app/lib/src/features/home/presentation/pages/listing_page.dart
+++ tradeit_app/lib/src/features/home/presentation/pages/listing_page.dart
@@ -2,11 +2,19 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tradeit_app/shared/widgets/custom_bottom_app_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tradeit_app/src/features/auth/domain/entities/app_user.dart';
 import 'package:tradeit_app/src/features/product_detail/presentation/pages/product_detail.dart';
+import 'package:tradeit_app/shared/globalUser.dart';
+
+final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+Future<Map<String, dynamic>?> getUser(String ownerId) async {
+  final doc = await FirebaseFirestore.instance.collection('users').doc(ownerId).get();
+  return doc.data();
+}
 
 
 class ListingPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,9 +32,11 @@ class ListingPage extends StatelessWidget {
           ),
         ],
       ),
-      body: StreamBuilder<QuerySnapshot>(stream: FirebaseFirestore.instance.collection('ads').snapshots(), 
-        builder: (context, snapshot){
-          if(snapshot.hasError) return Center(child: Text("Erro ${snapshot.error}"));
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('ads').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError)
+            return Center(child: Text("Erro ${snapshot.error}"));
 
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
@@ -37,43 +47,81 @@ class ListingPage extends StatelessWidget {
           }
           final firebaseData = snapshot.data!.docs;
 
-          return ListView.builder(itemCount: firebaseData.length, itemBuilder: (context, index){
-            var ad = firebaseData[index];
-            final data = ad.data() as Map<String, dynamic>?;
+          return ListView.builder(
+            itemCount: firebaseData.length,
+            itemBuilder: (context, index) {
+              var ad = firebaseData[index];
+              final data = ad.data() as Map<String, dynamic>?;
 
-            return Container(
-              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black, width: 1)
-              ),
-              child: ListTile(
-                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                subtitle: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                  
-                      Text(data?['title'], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
-                      SizedBox(height: 4),
-                      
-                      Text(data?['description']),
-                      SizedBox(height: 4),
-                      Text("Categoria: " + data?['category'], style: TextStyle(color: Colors.grey[700]))
-                    
-                    ],
-                  ),
-                ),
-                onTap: () {
-                  Navigator.pushNamed(context, '/details', arguments: {'adId': ad.id, 'ownerId':  data?['ownerId']});
+              return FutureBuilder<Map<String, dynamic>?>(
+                future: getUser(
+                  data?['ownerId'],
+                ), // sua função que busca o usuário
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+
+                  if (snapshot.hasError ||
+                      !snapshot.hasData ||
+                      snapshot.data == null) {
+                    return ListTile(title: Text('Erro ao carregar usuário'));
+                  }
+
+                  final user = snapshot.data!;
+                  final nome =
+                      user['name']; 
+
+                  return Container(
+                    margin: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            data?['title'] ?? '',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Text(data?['description'] ?? ''),
+                          SizedBox(height: 4),
+                          Text(
+                            "Criado por: $nome",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 4),
+                          Text("Categoria: ${data?['category'] ?? ''}"),
+                        ],
+                      ),
+                      onTap: () {
+                        Navigator.pushNamed(
+                          context,
+                          '/details',
+                          arguments: {
+                            'adId': ad.id,
+                            'ownerId': data?['ownerId'],
+                          },
+                        );
+                      },
+                    ),
+                  );
                 },
-              ),
-            );
-          });
-        }
-        
-      ), 
+              );
+            },
+          );
+        },
+      ),
       bottomNavigationBar: CustomBottomAppBar(currentIndex: 0),
-
     );
   }
 }
