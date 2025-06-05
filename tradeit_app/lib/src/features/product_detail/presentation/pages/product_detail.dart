49a4f0c80fb27b5366ea73da0e792d895aa5755d
Rafael Pereira <rafael_pereira5@outlook.com>
2025-06-05 06:35:37 -0300

    modal proposta

--- tradeit_app/lib/src/features/product_detail/presentation/pages/product_detail.dart
+++ tradeit_app/lib/src/features/product_detail/presentation/pages/product_detail.dart
@@ -5,8 +5,108 @@ import 'package:intl/intl.dart';
 import 'package:tradeit_app/shared/widgets/custom_bottom_app_bar.dart';
 import 'package:tradeit_app/src/features/product_detail/presentation/controller/produtController.dart';
 import '../../../../../shared/globalUser.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class ProductDetail extends StatelessWidget {
+
+final TextEditingController _textController = TextEditingController();
+  File? _imagemSelecionada;
+
+  void _abrirModal(BuildContext context, String ownerId, String ownerName, controller) {
+    final picker = ImagePicker();
+    File? imagemSelecionada;
+    final TextEditingController _controller = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            Future<void> _selecionarImagem() async {
+              final picked = await picker.pickImage(
+                source: ImageSource.gallery,
+              );
+              if (picked != null) {
+                setState(() {
+                  imagemSelecionada = File(picked.path);
+                });
+              }
+            }
+
+            Future<void> _enviarProposta(String ownerId, String ownerName, controller) async {
+              final uidAtual = currentUser?.id;
+              final nomeAtual = currentUser?.name ?? 'Usuário';
+              final outroUid = ownerId; // ID do dono do anúncio
+              final outroNome = controller.adData.value['userName'] ?? 'Outro';
+
+              if (_controller.text.trim().isEmpty) return;
+
+              String? imagemUrl;
+              // if (imagemSelecionada != null) {
+              //   final ref = FirebaseStorage.instance
+              //       .ref()
+              //       .child('propostas')
+              //       .child('${DateTime.now().millisecondsSinceEpoch}.jpg');
+              //   await ref.putFile(imagemSelecionada!);
+              //   imagemUrl = await ref.getDownloadURL();
+              // }
+
+              await FirebaseFirestore.instance.collection('inbox').add({
+                'proposta': currentUser!.name,
+                'ultimaMensagem': _controller.text.trim(),
+                'usuarios': [currentUser!.id, ownerId],
+                'nomes': [currentUser!.name, ownerName],
+                'timestamp': Timestamp.now(),
+                'imagemUrl': imagemUrl ?? '',
+              });
+
+              Navigator.popAndPushNamed(context, '/home');
+            }
+
+            return AlertDialog(
+              title: Text('Oferta para Anunciante:'),
+              content: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        labelText: "Faça sua oferta!",
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: _selecionarImagem,
+                      child: Text("Selecionar Imagem"),
+                    ),
+                    if (imagemSelecionada != null)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Image.file(imagemSelecionada!, height: 100),
+                      ),
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cancelar'),
+                ),
+                ElevatedButton(
+                  onPressed:(){ _enviarProposta(ownerId, ownerName, controller);},
+                  child: Text('Enviar'),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final args =
